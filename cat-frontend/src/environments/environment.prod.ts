9ba1dead030d55ef87ac9297565bd9e4398bc5bf
MarkGs <141935744+M3rl0n@users.noreply.github.com>
2025-08-26 19:25:27 -0500

    Actualizar environment.prod.ts

--- cat-frontend/src/environments/environment.prod.ts
+++ cat-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-production-api.com/api' // Cambiar por tu URL de producción
+  apiUrl: 'https://your-production-api.com/api'
 };
